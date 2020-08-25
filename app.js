0d5da6c7249a27501d09c019ba4188d6a4fcc1a8
gcatz <gendash@gmail.com>
2020-08-25 16:23:28 +0300

    Added a reusable component

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+Vue.component('player', {
+    template: 
+    `
+    <div>
+        <div>Hi, I'm a reusable player component!</div>
+    </div>
+    `
+});
+
 new Vue({
     el: '#my-app-root',
     data: {
